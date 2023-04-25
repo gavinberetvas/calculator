0e82e63dd6f4963972cf308f5db3e402d60490b7
metapersona <gavinberetvas@ucsb.edu>
2023-04-24 17:57:24 -0700

    +- button in progress

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let numbersArray2 = [];
 let display = 0;
 let operator = null;
 let answer = 0;
+let reset = null;
 
 function addToNumbersArray(number) {
   numbersArray.push(number);
@@ -18,22 +19,25 @@ for (let i = 0; i < 10; i++) {
 
 document.getElementById("equals").addEventListener("click", function () {
   math();
-
-  test();
-  //alert(`${answer}`);
+  equals();
+  //alert(`${reset}`);
 });
 
-document.getElementById("negate").addEventListener("click", function () {
-  let num1 = parseInt(numbersArray.join(""));
-  if  (num1 > 0) {
-    document.getElementById("screen").innerHTML = `${num1 - num1 - num1}`;
-  }
-});
 
 document.getElementById("plus").addEventListener("click", function () {
   if (operator == null) {
     operate("plus");
-  } else if (operator != null) {
+  } 
+  else if ((operator != null & reset != null)){ 
+    math(operator);
+    operator = "plus";
+    numbersArray2 = [];
+    numbersArray2.push(reset);
+    document.getElementById("second-screen").innerHTML = `${reset}`;
+    document.getElementById("screen").innerHTML = ``;
+    numbersArray = [];
+  }
+  else if (operator != null) {
     math(operator);
     operator = "plus";
     numbersArray2 = [];
@@ -41,13 +45,22 @@ document.getElementById("plus").addEventListener("click", function () {
     document.getElementById("second-screen").innerHTML = `${answer}`;
     document.getElementById("screen").innerHTML = ``;
     numbersArray = [];
-  }
+  } 
 });
 
 document.getElementById("minus").addEventListener("click", function () {
   if (operator == null) {
     operate("minus");
-  } else if (operator != null) {
+  } else if ((operator != null & reset != null)){ 
+    math(operator);
+    operator = "minus";
+    numbersArray2 = [];
+    numbersArray2.push(reset);
+    document.getElementById("second-screen").innerHTML = `${reset}`;
+    document.getElementById("screen").innerHTML = ``;
+    numbersArray = [];
+  }
+  else if (operator != null) {
     math(operator);
     operator = "minus";
     numbersArray2 = [];
@@ -61,7 +74,16 @@ document.getElementById("minus").addEventListener("click", function () {
 document.getElementById("times").addEventListener("click", function () {
   if (operator == null) {
     operate("times");
-  } else if (operator != null) {
+  } else if ((operator != null & reset != null)){ 
+    math(operator);
+    operator = "times";
+    numbersArray2 = [];
+    numbersArray2.push(reset);
+    document.getElementById("second-screen").innerHTML = `${reset}`;
+    document.getElementById("screen").innerHTML = ``;
+    numbersArray = [];
+  } 
+  else if (operator != null) {
     math(times);
     operator = "times";
     numbersArray2 = [];
@@ -75,7 +97,16 @@ document.getElementById("times").addEventListener("click", function () {
 document.getElementById("div").addEventListener("click", function () {
   if (operator == null) {
     operate("div");
-  } else if (operator != null) {
+  } else if ((operator != null & reset != null)){ 
+    math(operator);
+    operator = "div";
+    numbersArray2 = [];
+    numbersArray2.push(reset);
+    document.getElementById("second-screen").innerHTML = `${reset}`;
+    document.getElementById("screen").innerHTML = ``;
+    numbersArray = [];
+  } 
+  else if (operator != null) {
     math(div);
     operator = "div";
     numbersArray2 = [];
@@ -119,12 +150,13 @@ function math() {
   }
 }
 
-function test() {
+function equals() {
   document.getElementById("second-screen").innerHTML = ``;
   numbersArray = [];
   numbersArray2 = [];
-  //operator = null;
-  //answer = 0;
+  operator = 0;
+  reset = answer;
+  answer = 0;
 }
 
 function blank() {
@@ -134,3 +166,13 @@ function blank() {
   numbersArray2 = [];
   operator = 0;
 }
+
+function negate() {
+    if (reset != null) {
+      reset = reset - reset * 2
+      document.getElementById("screen").innerHTML = `${reset}`;
+    } else {
+      answer = answer - answer * 2
+      document.getElementById("screen").innerHTML = `${answer}`;
+    }
+  }
